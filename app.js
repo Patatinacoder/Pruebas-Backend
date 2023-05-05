fe31fc9011897c2cf98cae39142ba37d0f1d137d
Patatina <estudioh1chile@gmail.com>
2023-05-05 19:34:37 -0400

    acutalizo

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ import path from "path";
 import  {dirname}  from "path";
 import {fileURLToPath }from "url";
 import sessionRouter from './Routes/sessionRouter.js'
-import passport from "./Routes/passportRouter.js"
 
 dotenv.config();
 const app = express();
@@ -21,9 +20,6 @@ app.use('/api/carts', cartRouter);
 app.use('/', sessionRouter)
 
 
-// Configurar Passport
-app.use(passport.initialize());
-app.use(passport.session());
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', path.join(__dirname, 'views'));
